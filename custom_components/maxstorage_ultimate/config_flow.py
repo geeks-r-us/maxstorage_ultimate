cdfc278a69050ac2909e290424460129afab6571
geeks-r-us <root@geeks-r-us.de>
2024-01-16 23:38:34 +0000

    improved config_flow

--- custom_components/maxstorage_ultimate/config_flow.py
+++ custom_components/maxstorage_ultimate/config_flow.py
@@ -2,158 +2,208 @@
 from __future__ import annotations
 
 import logging
-from socket import gaierror, getaddrinfo, herror, timeout
+import socket
 from typing import Any
 
 import voluptuous as vol
-from voluptuous.schema_builder import Schema
 
-from homeassistant import config_entries
+from homeassistant.config_entries import ConfigEntry, ConfigFlow
 from homeassistant.const import CONF_NAME
-from homeassistant.core import HomeAssistant
+from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 
+from .client import AuthenticationFailedError, InvalidHostError, MaxStorageClient
 from .const import (
     CONF_STORAGE_HOST,
     CONF_STORAGE_PASSWORD,
-    CONF_STORAGE_PORT,
     CONF_STORAGE_USER,
+    CONF_STORAGE_VPN,
     DEFAULT_PASSWORD,
     DEFAULT_USER,
     DOMAIN,
+    ERROR_AUTH_INVALID,
+    ERROR_CANNOT_CONNECT,
+    ERROR_UNKNOWN,
     SENSOR_PREFIX,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def step_user_data_schema(
-    data: dict[str, Any] = {
-        CONF_NAME: SENSOR_PREFIX,
-        CONF_STORAGE_USER: DEFAULT_USER,
-        CONF_STORAGE_PASSWORD: DEFAULT_PASSWORD,
-    },
-) -> Schema:
-    _LOGGER.debug("config_flow.py:step_user_data_schema: %s", data)
-    STEP_USER_DATA_SCHEMA = vol.Schema(
-        {
-            vol.Required(CONF_NAME, default=data.get(CONF_NAME)): str,
-            vol.Required(CONF_STORAGE_HOST, default=data.get(CONF_STORAGE_HOST)): str,
-            vol.Required(CONF_STORAGE_USER, default=data.get(CONF_STORAGE_USER)): str,
-            vol.Required(
-                CONF_STORAGE_PASSWORD, default=data.get(CONF_STORAGE_PASSWORD)
-            ): str,
-        },
-        extra=vol.PREVENT_EXTRA,
-    )
-    _LOGGER.debug(
-        "config_flow.py:step_user_data_schema: STEP_USER_DATA_SCHEMA: %s",
-        STEP_USER_DATA_SCHEMA,
-    )
-    return STEP_USER_DATA_SCHEMA
+class MaxStorageFlowHandler(ConfigFlow, domain=DOMAIN):
+    """Handle a MaxStorage config flow."""
 
-
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
-    """Validate the user input allows us to connect.
-
-    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
-    """
-
-    _LOGGER.debug("config_flow.py:validate_input: %s", data)
-
-    try:
-        getaddrinfo(
-            data[CONF_STORAGE_HOST],
-            CONF_STORAGE_PORT,
-            family=0,
-            type=0,
-            proto=0,
-            flags=0,
-        )
-    except herror:
-        raise InvalidHost
-    except gaierror:
-        raise CannotConnect
-    except timeout:
-        raise CannotConnect
-
-    return {"title": data[CONF_STORAGE_HOST]}
-
-
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
+
+    def __init__(self) -> None:
+        """Initialize MaxStorage Ultimate flow."""
+        _LOGGER.debug("config_flow.py:MaxStorageFlowHandler.__init__")
+        self._host: str | None = None
+        self._name: str | None = None
+        self._user: str = ""
+        self._password: str = ""
+
+    async def maxstorage_ultimate_init(self) -> str | None:
+        """Initialize MaxStorage Ultimate."""
+        _LOGGER.debug("config_flow.py:MaxStorageFlowHandler.maxstorage_ultimate_init")
+
+        try:
+            client = MaxStorageClient(self._host, self._user, self._password)
+            await client.get_data()
+            await client.close()
+        except AuthenticationFailedError:
+            return ERROR_AUTH_INVALID
+        except InvalidHostError:
+            return ERROR_CANNOT_CONNECT
+        except Exception as e:  # pylint: disable=broad-except
+            _LOGGER.error("Unexpected exception: %s", e)
+            return ERROR_UNKNOWN
+
+        return None
+
+    async def async_check_configured_entry(self) -> ConfigEntry | None:
+        """Check if entry is configured."""
+        assert self._host
+        current_host = await self.hass.async_add_executor_job(
+            socket.gethostbyname, self._host
+        )
+
+        for entry in self._async_current_entries(include_ignore=False):
+            entry_host = await self.hass.async_add_executor_job(
+                socket.gethostbyname, entry.data[CONF_STORAGE_HOST]
+            )
+            if entry_host == current_host:
+                return entry
+        return None
+
+    @callback
+    def _async_create_entry(self) -> FlowResult:
+        """Async create flow handler entry."""
+        return self.async_create_entry(
+            title=self._name,
+            data={
+                CONF_STORAGE_HOST: self._host,
+                CONF_STORAGE_USER: self._user,
+                CONF_STORAGE_PASSWORD: self._password,
+            },
+        )
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
-        _LOGGER.debug("config_flow.py:ConfigFlow.async_step_user: %s", user_input)
+        """Handle a flow initiated by the user."""
+        _LOGGER.debug(
+            "config_flow.py:MaxStorageFlowHandler.async_step_user: %s", user_input
+        )
 
         if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=step_user_data_schema()
-            )
+            return self._show_setup_form_init()
 
         errors = {}
-        try:
-            info = await validate_input(self.hass, user_input)
-        except InvalidHost:
-            errors["base"] = "invalid_host"
-        except CannotConnect:
-            errors["base"] = "cannot_connect"
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
-        else:
-            _LOGGER.debug(
-                "config_flow.py:ConfigFlow.async_step_user: validation passed: %s",
-                user_input,
-            )
-            return self.async_create_entry(
-                title=info["title"], data=user_input, options=user_input
-            )
 
-        _LOGGER.debug(
-            "config_flow.py:ConfigFlow.async_step_user: validation failed: %s",
-            user_input,
-        )
+        self._name = user_input[CONF_NAME]
+        self._host = user_input[CONF_STORAGE_HOST]
+        self._user = user_input[CONF_STORAGE_USER]
+        self._password = user_input[CONF_STORAGE_PASSWORD]
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=step_user_data_schema(),
-            errors=errors,
-        )
+        if not (error := await self.maxstorage_ultimate_init()):
+            if await self.async_check_configured_entry():
+                error = "already_configured"
 
-    async def async_step_zeroconf(self, discovery_info):
+        if error:
+            return self._show_setup_form_init({"base": error})
+
+        return self._async_create_entry()
+
+    async def async_step_zeroconf(self, discovery_info) -> FlowResult:
         """Handle a discovered MaxStorage device via zeroconf."""
         _LOGGER.debug("Discovered MaxStorage device: %s", discovery_info)
 
         # Extract relevant information
-        properties = discovery_info.properties
-        artno = properties.get("artno")
-        serial = properties.get("serial")
-        vpn = properties.get("vpn")
+        self._host = discovery_info.hostname[
+            : -1 if discovery_info.hostname.endswith(".") else None
+        ]
+        self._name = discovery_info.name.replace("._maxstorage._tcp.local.", "")
+        self.context[CONF_STORAGE_HOST] = self._host
 
-        # You might want to check the service type as well to ensure it matches your device
-        service_type = discovery_info.get("type")
-        if service_type != "_maxstorage._tcp.local.":
-            return self.async_abort(reason="not_maxstorage_device")
+        if uuid := discovery_info.name:
+            await self.async_set_unique_id(uuid)
+            self._abort_if_unique_id_configured({CONF_STORAGE_HOST: self._host})
 
-        await self.async_set_unique_id(serial)
-        self._abort_if_unique_id_configured()
+        for progress in self._async_in_progress():
+            if progress.get("context", {}).get(CONF_STORAGE_HOST) == self._host:
+                return self.async_abort(reason="already_in_progress")
 
-        # Here, the serial number is used to set a unique ID for each device
-        # You can then use this unique ID to avoid creating duplicate entries for the same device
-        return self.async_show_form(
-            step_id="user",
-            data_schema=step_user_data_schema(),
-            description_placeholders={"artno": artno, "serial": serial, "vpn": vpn},
+        if entry := await self.async_check_configured_entry():
+            if uuid and not entry.unique_id:
+                self.hass.config_entries.async_update_entry(entry, unique_id=uuid)
+            return self.async_abort(reason="already_configured")
+
+        self.context.update(
+            {
+                "title_placeholders": {"name": self._name},
+                "configuration_url": f"http://{self._host}",
+            }
         )
 
+        return await self.async_step_confirm()
 
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
+    async def async_step_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle user-confirmation of discovered node."""
+        _LOGGER.debug(
+            "config_flow.py:MaxStorageFlowHandler.async_step_confirm: %s", user_input
+        )
 
+        if user_input is None:
+            return self._show_setup_form_confirm()
 
-class InvalidHost(HomeAssistantError):
-    """Error to indicate there is invalid hostname or IP address."""
+        errors = {}
+
+        self._user = user_input[CONF_STORAGE_USER]
+        self._password = user_input[CONF_STORAGE_PASSWORD]
+
+        error = await self.maxstorage_ultimate_init()
+
+        if error:
+            errors["base"] = error
+            return self._show_setup_form_confirm(errors)
+
+        return self._async_create_entry()
+
+    def _show_setup_form_init(self, errors: dict[str, str] | None = None) -> FlowResult:
+        """Show the setup form to the user."""
+        _LOGGER.debug(
+            "config_flow.py:MaxStorageFlowHandler._show_setup_form_init: %s", errors
+        )
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_NAME, default=SENSOR_PREFIX): str,
+                    vol.Required(CONF_STORAGE_HOST): str,
+                    vol.Required(CONF_STORAGE_USER, default=DEFAULT_USER): str,
+                    vol.Required(CONF_STORAGE_PASSWORD, default=DEFAULT_PASSWORD): str,
+                },
+                extra=vol.PREVENT_EXTRA,
+            ),
+            errors=errors or {},
+        )
+
+    def _show_setup_form_confirm(
+        self, errors: dict[str, str] | None = None
+    ) -> FlowResult:
+        """Show the setup form to the user."""
+        return self.async_show_form(
+            step_id="confirm",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_STORAGE_USER, default=DEFAULT_USER): str,
+                    vol.Required(CONF_STORAGE_PASSWORD, default=DEFAULT_PASSWORD): str,
+                }
+            ),
+            description_placeholders={"name": self._name},
+            errors=errors or {},
+        )
--- custom_components/maxstorage_ultimate/const.py
+++ custom_components/maxstorage_ultimate/const.py
@@ -7,5 +7,10 @@ CONF_STORAGE_HOST = "maxstorage_host"
 CONF_STORAGE_PORT = 80
 CONF_STORAGE_USER = "maxstorage_user"
 CONF_STORAGE_PASSWORD = "maxstorage_password"
+CONF_STORAGE_VPN = "maxstorage_vpn"
 
 SENSOR_PREFIX = "MaxStorage"
+
+ERROR_AUTH_INVALID = "invalid_auth"
+ERROR_CANNOT_CONNECT = "cannot_connect"
+ERROR_UNKNOWN = "unknown"
